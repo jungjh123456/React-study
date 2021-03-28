fe898064056f367e57e5c9348784eea1cef4e183
jungjh123456 <jungjh123456@naver.com>
2021-03-28 23:06:58 +0900

    Algorizem

--- react-pratice/src/components/Example2.jsx
+++ react-pratice/src/components/Example2.jsx
@@ -1,32 +1,22 @@
 import React from 'react';
 import Example3 from './Example3';
 
-const Example2 = React.memo(({state, setState}) => {
-  return (
-    <div>
-      <Example3 state={state} setState={setState}/>
-      <h1>
-      hellomen
-      </h1>
-    </div>
-  );
-},(prev, next) => {
-  if (prev.state.count === next.state.count) {
-    return true;
-  } else {
-    return true;
-  }
-});
+const Example2 = React.memo(
+	({ state, setState }) => {
+		return (
+			<div>
+				<Example3 state={state} setState={setState} />
+				<h1>hellomen</h1>
+			</div>
+		);
+	},
+	(prev, next) => {
+		if (prev.state.count === next.state.count) {
+			return true;
+		} else {
+			return true;
+		}
+	}
+);
 
-
-
-// const Example2 = () => {
-//   return (
-//     <div>
-//       hel1
-//     </div>
-//   );
-
-//   }
-
-export default Example2;  
+export default Example2;
